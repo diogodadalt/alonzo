7000fd7e0101996c7799973e83e4df2b2a5e9bd5
Diogo Dadalt <diogodadalt@gmail.com>
2014-10-25 20:28:11 -0200

    fixing problem with the mocha plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,10 +42,11 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-watch');  
 
   grunt.registerTask('test', ['jshint', 'simplemocha']);
   grunt.registerTask('default', ['jshint', 'simplemocha', 'concat', 'uglify']);
